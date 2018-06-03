fafc10144560fbd195d0e85e470d5e9060b0a538
yiyunzhu <zhu.yiy@husky.neu.edu>
2018-06-03 12:07:23 -0700

    added index,CourseManager, install bootstrap

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CourseManager extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Course Manager</h1>
+            </div>
+        );
+    }
+}
+
+export default CourseManager;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CourseManager from './containers/CourseManager'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+    <CourseManager/>,
+    document.getElementById('root')
+);
